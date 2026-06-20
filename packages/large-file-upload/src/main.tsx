34a16b1531cf5ab28676737cf1de298b696d1792
高艳兵 <yanbing.gao@aliyun.com>
2026-06-20 19:16:47 +0800

    docs: document frontend mount

--- packages/large-file-upload/src/main.tsx
+++ packages/large-file-upload/src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+/**
+ * Mounts the upload demo into the Vite HTML root element.
+ */
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
